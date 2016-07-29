bd98d5ffd7866ee179fd5e324ba94443101df9fc
Alexander Trakhimenok <astec@users.noreply.github.com>
2016-07-30 00:22:21 +0100

    Logger interface

---
+++ logger.go
@@ -0,0 +1,9 @@
+package app
+
+type Logger interface {
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Warningf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Criticalf(format string, args ...interface{})
+}
