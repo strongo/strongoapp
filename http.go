ebda240ff3e56970fabea715e8fc23b5268c6bc2
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2016-08-24 22:17:51 +0100

    Use strongo.AddHttpHandler() to handle closing "/". Refactoring & cleanup.

---
+++ http.go
@@ -0,0 +1,13 @@
+package strongo
+
+import (
+	"net/http"
+	"strings"
+)
+
+func AddHttpHandler(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	http.HandleFunc(pattern, handler)
+	if !strings.HasSuffix(pattern, "/") {
+		http.HandleFunc(pattern + "/", handler)
+	}
+}
