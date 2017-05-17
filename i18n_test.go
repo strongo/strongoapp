76c42ef3a7dcefd03218cf64d960e9f5ab1ae0df
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2017-05-17 17:15:43 +0100

    Small changes

--- i18n_test.go
+++ i18n_test.go
@@ -1,12 +1,11 @@
 package strongo
 
 import (
-	"github.com/strongo/app"
 	"testing"
 )
 
 func TestLocale_String(t *testing.T) {
-	l := strongo.Locale{Code5: "c0de5", IsRtl: true, NativeTitle: "Код05", EnglishTitle: "C0de5", FlagIcon: "fl0g"}
+	l := Locale{Code5: "c0de5", IsRtl: true, NativeTitle: "Код05", EnglishTitle: "C0de5", FlagIcon: "fl0g"}
 	actualLs := l.String()
 	expectingLs := `Locale{Code5: "c0de5", IsRtl: true, NativeTitle: "Код05", EnglishTitle: "C0de5", FlagIcon: "fl0g"}`
 	if actualLs != expectingLs {
