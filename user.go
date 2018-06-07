b545cb06514e4d42aca80fbd522a0407a4f35f81
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-06-07 01:36:32 +0100

    step-by-step (time to sleep)

--- user.go
+++ user.go
@@ -20,6 +20,22 @@ type AppUserBase struct {
 	UserName  string `datastore:",noindex"`
 }
 
+func (u *AppUserBase) FullName() string {
+	if u.FirstName != "" && u.LastName != "" {
+		return u.FirstName + " " + u.LastName
+	}
+	if u.FirstName != "" {
+		return u.FirstName
+	}
+	if u.LastName != "" {
+		return u.LastName
+	}
+	if u.UserName != "" {
+		return u.UserName
+	}
+	return ""
+}
+
 var _ AppUser = (*AppUserBase)(nil)
 
 // SetPreferredLocale sets preferred locale
