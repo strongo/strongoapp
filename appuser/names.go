778748d2b71c9fc5f1c0df8218fc7f705db0d5fc
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-11-24 22:37:40 +0000

    feat(Name): FullName

--- appuser/names.go
+++ appuser/names.go
@@ -10,6 +10,7 @@ const (
 	MiddleName
 	LastName
 	NickName
+	FullName
 )
 
 type Name struct {
@@ -24,26 +25,29 @@ type NamesSetter interface {
 var _ NamesSetter = (*NameFields)(nil)
 
 type NameFields struct {
+	UserName   string `json:"userName,omitempty" dalgo:"userName,omitempty" firestore:"userName,omitempty"`
 	FirstName  string `json:"firstName,omitempty" dalgo:"firstName,omitempty" firestore:"firstName,omitempty"`
 	MiddleName string `json:"middleName,omitempty" dalgo:"middleName,omitempty" firestore:"middleName,omitempty"`
 	LastName   string `json:"lastName,omitempty" dalgo:"lastName,omitempty" firestore:"lastName,omitempty"`
 	NickName   string `json:"nickName,omitempty" dalgo:"nickName,omitempty" firestore:"nickName,omitempty"`
-	UserName   string `json:"userName,omitempty" dalgo:"userName,omitempty" firestore:"userName,omitempty"`
+	FullName   string `json:"fullName,omitempty" dalgo:"fullName,omitempty" firestore:"fullName,omitempty"`
 }
 
 func (u *NameFields) SetNames(names ...Name) {
 	for _, name := range names {
 		switch name.Field {
+		case Username:
+			u.UserName = name.Value
 		case FirstName:
 			u.FirstName = name.Value
 		case MiddleName:
 			u.MiddleName = name.Value
 		case LastName:
 			u.LastName = name.Value
-		case Username:
-			u.UserName = name.Value
 		case NickName:
 			u.NickName = name.Value
+		case FullName:
+			u.FullName = name.Value
 		default:
 			panic(fmt.Sprintf("unsupported name field: %d", name.Field))
 		}
@@ -66,20 +70,37 @@ func (u *NameFields) GetName(field name) string {
 		return u.UserName
 	case NickName:
 		return u.NickName
+	case FullName:
+		return u.GetFullName()
 	default:
 		return ""
 	}
 }
 
 func (u *NameFields) GetFullName() string {
+	if u.FullName != "" {
+		return u.FullName
+	}
+	if u.FirstName != "" && u.NickName != "" && u.LastName != "" {
+		return fmt.Sprintf("%s (%s) %s", u.FirstName, u.NickName, u.LastName)
+	}
 	if u.FirstName != "" && u.LastName != "" {
 		return u.FirstName + " " + u.LastName
+	}
+	if u.FirstName != "" && u.NickName != "" {
+		return fmt.Sprintf("%s (%s) %s", u.FirstName, u.NickName)
+	}
+	if u.LastName != "" && u.NickName != "" {
+		return fmt.Sprintf("%s (%s) %s", u.LastName, u.NickName)
 	}
 	if u.FirstName != "" {
 		return u.FirstName
 	}
 	if u.LastName != "" {
 		return u.LastName
+	}
+	if u.NickName != "" {
+		return u.NickName
 	}
 	if u.UserName != "" {
 		return u.UserName
