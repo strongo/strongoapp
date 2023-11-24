466139130d24faf054a95408885c1a4eef337f33
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-11-24 12:59:46 +0000

    feat(user): SetNames() now uses struct arg + tests

--- user.go
+++ user.go
@@ -1,27 +1,32 @@
 package strongo
 
 import (
+	"fmt"
 	"github.com/strongo/app/user"
 	"time"
 )
+
+type Names struct {
+	FirstName *string
+	LastName  *string
+	UserName  *string
+}
 
 // AppUser defines app user interface
 type AppUser interface {
 	SetPreferredLocale(code5 string) error
 	GetPreferredLocale() string
 
-	SetNames(first, last, user string)
+	SetNames(names Names)
 	//GetCurrencies() []string
 }
 
 // AppUserBase hold common properties for AppUser interface
 type AppUserBase struct {
+	UserNames
 	DtCreated time.Time
 	user.AccountsOfUser
-	Locale    string `datastore:",noindex"`
-	FirstName string `datastore:",noindex"`
-	LastName  string `datastore:",noindex"`
-	UserName  string `datastore:",noindex"`
+	Locale string `datastore:",noindex"`
 }
 
 func (u *AppUserBase) GetFullName() string {
@@ -53,9 +58,25 @@ func (u *AppUserBase) GetPreferredLocale() string {
 	return u.Locale
 }
 
+type UserNames struct {
+	FirstName string `datastore:",noindex"`
+	LastName  string `datastore:",noindex"`
+	UserName  string `datastore:",noindex"`
+}
+
+func (u *UserNames) String() string {
+	return fmt.Sprintf(`{UserName="%s", FirstName="%s", LastName="%s"}`, u.UserName, u.FirstName, u.LastName)
+}
+
 // SetNames sets names
-func (u *AppUserBase) SetNames(first, last, user string) {
-	u.FirstName = first
-	u.LastName = last
-	u.UserName = user
+func (u *UserNames) SetNames(names Names) {
+	if names.FirstName != nil {
+		u.FirstName = *names.FirstName
+	}
+	if names.LastName != nil {
+		u.LastName = *names.LastName
+	}
+	if names.UserName != nil {
+		u.UserName = *names.UserName
+	}
 }
---
+++ user_test.go
@@ -0,0 +1,79 @@
+package strongo
+
+import (
+	"github.com/strongo/app/user"
+	"testing"
+	"time"
+)
+
+func TestUserNames_SetNames(t *testing.T) {
+	type fields struct {
+		DtCreated      time.Time
+		AccountsOfUser user.AccountsOfUser
+		Locale         string
+		FirstName      string
+		LastName       string
+		UserName       string
+	}
+	type args struct {
+		names Names
+	}
+	strPtr := func(s string) *string {
+		return &s
+	}
+	tests := []struct {
+		name     string
+		user     UserNames
+		args     args
+		expected UserNames
+	}{
+		{
+			name: "first_name",
+			user: UserNames{FirstName: "Jack", LastName: "Doe"},
+			args: args{
+				names: Names{
+					FirstName: strPtr("John"),
+				},
+			},
+			expected: UserNames{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+		},
+		{
+			name: "last_name",
+			user: UserNames{FirstName: "Jack", LastName: "Doe"},
+			args: args{
+				names: Names{
+					LastName: strPtr("Jones"),
+				},
+			},
+			expected: UserNames{
+				FirstName: "Jack",
+				LastName:  "Jones",
+			},
+		},
+		{
+			name: "user_name",
+			user: UserNames{FirstName: "Jack", LastName: "Doe"},
+			args: args{
+				names: Names{
+					UserName: strPtr("joker"),
+				},
+			},
+			expected: UserNames{
+				FirstName: "Jack",
+				LastName:  "Doe",
+				UserName:  "joker",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.user.SetNames(tt.args.names)
+			if tt.user != tt.expected {
+				t.Errorf("AppUserBase.SetNames() = %v, want %v", tt.user, tt.expected)
+			}
+		})
+	}
+}
